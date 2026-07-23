c146cfcdd61ad46ad03d3a7b52904c8fb64cc694
ArniSri257 <arnishrivastava123@gmail.com>
2026-07-23 05:38:24 +0000

    Added Hello World program

---
+++ JAVA01/Hello.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class Hello
+	{
+		public static void main(String arg[])
+		{
+			System.out.println("Hello Java");
+			System.out.println("This is my first Java program");
+			System.out.print("My name is Arni Srivastava");
+		}
